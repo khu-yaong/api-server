1179926db261f95356386c3aed370faa95726760
jaehyyuun <jaehyunj393@gmail.com>
2024-11-04 13:42:25 +0900

    [chore] Initialize Spring Boot project with Gradle

---
+++ src/main/java/com/khu/yaong/YaongApplication.java
@@ -0,0 +1,13 @@
+package com.khu.yaong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YaongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khu/yaong/YaongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khu.yaong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
