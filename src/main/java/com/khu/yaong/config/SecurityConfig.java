aefeba530fe11ffb8561c621300dd5083e519efc
jaehyyuun <jaehyunj393@gmail.com>
2024-11-06 13:01:10 +0900

    [chore] Enable CORS configuration

--- src/main/java/com/khu/yaong/config/SecurityConfig.java
+++ src/main/java/com/khu/yaong/config/SecurityConfig.java
@@ -3,10 +3,10 @@ package com.khu.yaong.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,6 +17,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize
--- src/main/java/com/khu/yaong/config/SwaggerConfig.java
+++ src/main/java/com/khu/yaong/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package com.khu.yaong.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
