be83379baad0f81955906964b7dd9e2f433994bf
jaehyyuun <jaehyunj393@gmail.com>
2024-11-12 00:31:49 +0900

    [feat] Add custom response codes for Member exceptions

--- src/main/java/com/khu/yaong/global/common/response/member/MemberSuccessCode.java
+++ src/main/java/com/khu/yaong/global/common/response/member/MemberSuccessCode.java
@@ -1,22 +1,22 @@
-package com.khu.yaong.global.common.response;
-
+package com.khu.yaong.global.common.response.member;
 
 import com.khu.yaong.global.common.type.SuccessResponse;
 import org.springframework.http.HttpStatus;
 
-public enum SuccessCode implements SuccessResponse {
-    SUCCESS(HttpStatus.OK,"SUCCESS", "요청이 성공적으로 처리되었습니다."),
-    CREATED(HttpStatus.CREATED, "CREATED", "새 리소스가 생성되었습니다.");
+public enum MemberSuccessCode implements SuccessResponse {
+    REGISTER_SUCCESS(HttpStatus.CREATED,"MEMBER_CREATED", "회원가입이 성공적으로 이루어졌습니다."),
+    LOGIN_SUCCESS(HttpStatus.OK,"LOGIN_SUCCESS", "로그인이 성공적으로 이루어졌습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 
-    SuccessCode(HttpStatus httpStatus, String code, String message) {
+    MemberSuccessCode(HttpStatus httpStatus, String code, String message) {
         this.httpStatus = httpStatus;
         this.code = code;
         this.message = message;
     }
+
     @Override
     public HttpStatus getHttpStatus() {
         return httpStatus;
@@ -32,3 +32,4 @@ public enum SuccessCode implements SuccessResponse {
         return message;
     }
 }
+
