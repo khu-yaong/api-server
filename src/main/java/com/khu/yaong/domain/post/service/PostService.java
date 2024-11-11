165c8e297fa85a233af114574150bf1cc6dbb9a8
dvlp-sy <sy8389b@gmail.com>
2024-11-11 23:10:43 +0900

    [feat] Implement createPost

--- src/main/java/com/khu/yaong/domain/post/service/PostService.java
+++ src/main/java/com/khu/yaong/domain/post/service/PostService.java
@@ -1,8 +1,13 @@
 package com.khu.yaong.domain.post.service;
 
+import com.khu.yaong.domain.post.dto.PostReqDTO;
+import com.khu.yaong.domain.post.dto.PostResDTO;
+
 public interface PostService {
 
 /*---------------------------------------- 게시글 ----------------------------------------*/
+
+    PostResDTO.PostDetailDTO createPost(PostReqDTO.PostDTO postDTO);
 
 /*---------------------------------------- 좋아요 ----------------------------------------*/
 
--- src/main/java/com/khu/yaong/domain/post/service/PostServiceImpl.java
+++ src/main/java/com/khu/yaong/domain/post/service/PostServiceImpl.java
@@ -1,6 +1,10 @@
 package com.khu.yaong.domain.post.service;
 
+import com.khu.yaong.domain.member.domain.Member;
 import com.khu.yaong.domain.member.repository.MemberRepository;
+import com.khu.yaong.domain.post.domain.Post;
+import com.khu.yaong.domain.post.dto.PostReqDTO;
+import com.khu.yaong.domain.post.dto.PostResDTO;
 import com.khu.yaong.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,6 +17,28 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
 
 /*---------------------------------------- 게시글 ----------------------------------------*/
+
+    @Override
+    public PostResDTO.PostDetailDTO createPost(PostReqDTO.PostDTO postDTO) {
+
+        // Authorization 구현 후 수정 예정
+        Member author = memberRepository.findById(1L)
+                .orElseThrow(RuntimeException::new);
+
+        Post post = Post.builder()
+                .author(author)
+                .title(postDTO.getTitle())
+                .content(postDTO.getContent())
+                .imageUrl(postDTO.getImageUrl())
+                .countLike(0L)
+                .countComment(0L)
+                .build();
+
+        Post savedPost = postRepository.save(post);
+        author.addPost(savedPost);
+
+        return PostResDTO.PostDetailDTO.toDTO(savedPost);
+    }
 
 /*---------------------------------------- 좋아요 ----------------------------------------*/
 
