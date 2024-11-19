1391b7daa303d7f8d2f48bf9bc21bb935caefbde
jaehyyuun <jaehyunj393@gmail.com>
2024-11-19 18:50:31 +0900

    [fix] Fix MemberProfile

--- src/main/java/com/khu/yaong/domain/member/controller/MemberProfileController.java
+++ src/main/java/com/khu/yaong/domain/member/controller/MemberProfileController.java
@@ -3,17 +3,17 @@ package com.khu.yaong.domain.member.controller;
 import com.khu.yaong.domain.member.dto.MemberProfileResponseDto;
 import com.khu.yaong.domain.member.exception.MemberNotFoundException;
 import com.khu.yaong.domain.member.service.MemberProfileService;
-import com.khu.yaong.global.auth.CustomUserDetails;
+import com.khu.yaong.global.security.CustomUserDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/users")
 public class MemberProfileController {
 
     private final MemberProfileService memberProfileService;
@@ -26,11 +26,13 @@ public class MemberProfileController {
     @GetMapping("/me")
     public ResponseEntity<MemberProfileResponseDto> getMemberProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
         try {
-            Long authenticatedMemberId = userDetails.getId();  // 인증된 사용자 ID 가져오기
-            MemberProfileResponseDto profile = memberProfileService.getMemberProfileById(authenticatedMemberId);
+            String authenticatedMemberId = userDetails.getUsername();  // 인증된 사용자 ID 가져오기
+            log.info(authenticatedMemberId.toString());
+            MemberProfileResponseDto profile = memberProfileService.getMemberProfileById(Long.valueOf(authenticatedMemberId));
             return ResponseEntity.ok(profile);
         } catch (MemberNotFoundException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+
     }
 }
