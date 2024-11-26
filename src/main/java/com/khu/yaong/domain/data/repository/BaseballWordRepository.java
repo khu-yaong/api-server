4e4aa0bc063a3170aef523cd497fe79308c1cc49
dvlp-sy <sy8389b@gmail.com>
2024-11-26 16:06:32 +0900

    [add] Add BaseballWordRepositoryCustom

--- src/main/java/com/khu/yaong/domain/data/repository/BaseballWordRepository.java
+++ src/main/java/com/khu/yaong/domain/data/repository/BaseballWordRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BaseballWordRepository extends JpaRepository<BaseballWord, Long> {
+public interface BaseballWordRepository extends JpaRepository<BaseballWord, Long>, BaseballWordRepositoryCustom {
 }
