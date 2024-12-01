4365119a976f88e582262c930ba92d2eae902fe2
dvlp-sy <sy8389b@gmail.com>
2024-12-01 15:27:17 +0900

    [fix] S3 Exception Handling

--- src/main/java/com/khu/yaong/global/s3/S3ImageService.java
+++ src/main/java/com/khu/yaong/global/s3/S3ImageService.java
@@ -35,6 +35,9 @@ public class S3ImageService {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
+    @Value("${cloud.aws.s3.default-profile}")
+    private String defaultProfile;
+
     public String uploadImage(String dir, MultipartFile image) {
         // image가 비어있으면 오류
         if (image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
@@ -102,6 +105,9 @@ public class S3ImageService {
 
     public void deleteImageFromS3(String imageUrl) {
         if (imageUrl == null || imageUrl.isEmpty()) {
+            return ;
+        }
+        if (imageUrl.equals(defaultProfile)) {
             return ;
         }
         String key = getKeyFromImageAddress(imageUrl);
