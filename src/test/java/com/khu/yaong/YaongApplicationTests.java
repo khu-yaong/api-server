176059579088bf16ea14825126536be61daeeb6c
Jihyeon An <jh011007@naver.com>
2024-12-01 18:08:20 +0900

    delete diary custom repository

--- src/test/java/com/khu/yaong/YaongApplicationTests.java
+++ src/test/java/com/khu/yaong/YaongApplicationTests.java
@@ -1,13 +1,13 @@
-package com.khu.yaong;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class YaongApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.khu.yaong;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class YaongApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
