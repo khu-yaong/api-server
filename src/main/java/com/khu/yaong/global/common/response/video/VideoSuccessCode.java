fa359040fe85e325895dd076bbbfce66cd61e82f
dvlp-sy <sy8389b@gmail.com>
2024-12-06 03:22:31 +0900

    [add] Add Success Code

--- src/main/java/com/khu/yaong/global/common/response/video/VideoSuccessCode.java
+++ src/main/java/com/khu/yaong/global/common/response/video/VideoSuccessCode.java
@@ -7,6 +7,7 @@ public enum VideoSuccessCode implements SuccessResponse {
 
     VIDEO_VIEW_LIST_UPDATED(HttpStatus.CREATED,"VIDEO2001", "영상 조회 내역이 업데이트 되었습니다."),
     RECOMMENDED_VIDEOS_FOUND(HttpStatus.OK, "VIDEO2002", "추천 영상 목록 조회에 성공하였습니다."),
+    TEAM_VIDEOS_FOUND(HttpStatus.OK, "VIDEO2003", "구단별 영상 목록 조회에 성공했하였습니다."),
 
     ;
 
